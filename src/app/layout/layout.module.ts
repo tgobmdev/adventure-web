00da6f28ec5a83e6e5b12a15744a4dcb5b69b7db
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-04 16:09:13 -0300

    feat: add content-section component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ContentSectionComponent } from './components/content-section/content-section.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LayoutComponent } from './layout.component';
 
 @NgModule({
-  declarations: [NavbarComponent, LayoutComponent, FooterComponent],
+  declarations: [
+    NavbarComponent,
+    LayoutComponent,
+    FooterComponent,
+    ContentSectionComponent,
+  ],
   imports: [CommonModule],
   exports: [LayoutComponent],
 })
