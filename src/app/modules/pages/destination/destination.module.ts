6693844aaa97736a4c6229a35ca2b0ec2d2c0418
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-06 14:22:14 -0300

    feat: add travel, destination modules

---
+++ src/app/modules/pages/destination/destination.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { DestinationRoutingModule } from './destination-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, DestinationRoutingModule],
+})
+export class DestinationModule {}
