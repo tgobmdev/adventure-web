5f717fbff217a32ce5fe271c87ab5876716728e9
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-07 11:39:02 -0300

    refactor: removed html, ts, scss account

--- src/app/modules/pages/account/account.module.ts
+++ src/app/modules/pages/account/account.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { CardModule } from 'primeng/card';
 import { AccountRoutingModule } from './account-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, AccountRoutingModule],
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, CardModule, AccountRoutingModule],
 })
 export class AccountModule {}
