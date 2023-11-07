2a8262714d367f54c2ed69467df23b07f0016134
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-06 21:38:14 -0300

    refactor: ajusted package home-page

--- src/app/modules/layout/layout-routing.module.ts
+++ src/app/modules/layout/layout-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from '../pages/home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 const HOME_PAGE = {
   path: 'home',
