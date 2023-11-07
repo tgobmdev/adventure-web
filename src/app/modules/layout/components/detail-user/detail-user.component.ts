cdbe19eb2748a8df86c9f8d313f40812b1df0d06
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-06 21:53:24 -0300

    feat: add detail-user component

---
+++ src/app/modules/layout/components/detail-user/detail-user.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-user',
+  templateUrl: './detail-user.component.html',
+  styleUrls: ['./detail-user.component.scss'],
+})
+export class DetailUserComponent {}
