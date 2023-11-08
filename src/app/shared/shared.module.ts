13dd4058fb29a2ad0286598c6493189e93ced155
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-07 23:53:32 -0300

    style: refactor css button component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { ButtonComponent } from './components/button/button.component';
 import { CardFormComponent } from './components/card-form/card-form.component';
 
 @NgModule({
   declarations: [CardFormComponent, ButtonComponent],
-  imports: [CommonModule, CardModule, ReactiveFormsModule],
+  imports: [CommonModule, ButtonModule, CardModule, ReactiveFormsModule],
   exports: [CardFormComponent, ButtonComponent],
 })
 export class SharedModule {}
