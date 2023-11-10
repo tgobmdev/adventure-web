e467a1b88aa10018428d467d6f524164a1658250
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-09 22:59:53 -0300

    refactor: add @ViewChield for focus video

--- src/app/modules/pages/welcome/components/welcome-video/welcome-video.component.ts
+++ src/app/modules/pages/welcome/components/welcome-video/welcome-video.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-video',
   templateUrl: './welcome-video.component.html',
   styleUrls: ['./welcome-video.component.scss'],
 })
-export class WelcomeVideoComponent {}
+export class WelcomeVideoComponent implements AfterViewInit {
+  @ViewChild('srcVideo') srcVideo!: ElementRef;
+  ngAfterViewInit(): void {
+    if (this.srcVideo.nativeElement) {
+      this.srcVideo.nativeElement.autofocus = true;
+    }
+  }
+}
