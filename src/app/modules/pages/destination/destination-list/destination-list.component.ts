122c11fb4e6f492e888631bef28c7daa70b2d006
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-19 16:21:07 -0300

    feat: add message error

--- src/app/modules/pages/destination/destination-list/destination-list.component.ts
+++ src/app/modules/pages/destination/destination-list/destination-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
 import { Columns } from '../../../../shared/components/table/models/columns';
 import { DestinationService } from '../destination.service';
 import { DestinationResponse } from '../dto/destination-response';
@@ -12,7 +13,10 @@ export class DestinationListComponent implements OnInit {
   cols!: Columns[];
   destinations!: DestinationResponse[];
 
-  constructor(private readonly destinationService: DestinationService) {}
+  constructor(
+    private readonly messageService: MessageService,
+    private readonly destinationService: DestinationService,
+  ) {}
 
   ngOnInit(): void {
     this.createTableColumns();
@@ -33,8 +37,17 @@ export class DestinationListComponent implements OnInit {
   };
 
   findAllDestinations = () => {
-    this.destinationService.getAllDestinations().subscribe((destinations) => {
-      this.destinations = destinations;
-    });
+    this.destinationService.getAllDestinations().subscribe(
+      (destinations) => {
+        this.destinations = destinations;
+      },
+      (error) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: error,
+        });
+      },
+    );
   };
 }
