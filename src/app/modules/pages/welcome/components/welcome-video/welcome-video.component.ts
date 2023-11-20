106c293793619683f1c4ee843b53b54c26723423
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-20 17:40:43 -0300

    fix: lazy load video

--- src/app/modules/pages/welcome/components/welcome-video/welcome-video.component.ts
+++ src/app/modules/pages/welcome/components/welcome-video/welcome-video.component.ts
@@ -1,15 +1,39 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-welcome-video',
   templateUrl: './welcome-video.component.html',
   styleUrls: ['./welcome-video.component.scss'],
 })
-export class WelcomeVideoComponent implements AfterViewInit {
+export class WelcomeVideoComponent implements OnInit, AfterViewInit {
   @ViewChild('srcVideo') srcVideo!: ElementRef;
+  videoLoaded = false;
+
+  ngOnInit(): void {
+    if (!this.videoLoaded && this.srcVideo && this.srcVideo.nativeElement) {
+      this.loadVideo();
+    }
+  }
+
   ngAfterViewInit(): void {
     if (this.srcVideo.nativeElement) {
       this.srcVideo.nativeElement.autofocus = true;
     }
   }
+
+  loadVideo = (): void => {
+    const videoElement = this.srcVideo.nativeElement;
+    videoElement.src = 'assets/videos/mp4/maragogi.mp4';
+    videoElement.addEventListener('loadeddata', () => {
+      this.videoLoaded = true;
+    });
+    videoElement.loop = true;
+    videoElement.play();
+  };
 }
